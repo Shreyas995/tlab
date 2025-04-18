4f394ed7a8c2056f6098785ade12139d78c61f51
Shreyas <shreyasdeshpande16@gmail.com>
2025-04-18 16:11:03 +0200

    Profiling time calculation for APU architecture

--- src/base/tlab_time.f90
+++ src/base/tlab_time.f90
@@ -5,4 +5,10 @@ module TLab_Time
     integer(wi) :: itime                ! iteration number
     real(wp) :: rtime                   ! physical time
 
+! ###################################################################
+! APU profiling timings
+! ###################################################################    
+    real(wp) :: trans_time, tridss_time, tridpss_time
+    real(wp) :: mat5dantisym_time,mat5dsym_time,mat3dadd_time,mat3d_time
+
 end module TLab_Time
