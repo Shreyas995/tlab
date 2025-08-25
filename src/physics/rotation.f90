9811ae601454cb2b416d2fc7b8697b61a3155e4d
Shreyas <shreyasdeshpande16@gmail.com>
2025-08-25 11:33:43 +0200

    removed old openMp directives

--- src/physics/rotation.f90
+++ src/physics/rotation.f90
@@ -127,8 +127,8 @@ contains
             geo_u = cos(locProps%parameters(1))*locProps%parameters(2)
             geo_w = -sin(locProps%parameters(1))*locProps%parameters(2)
 
-!$omp parallel default( shared ) &
-!$omp private( ij, dummy,srt,end,siz )
+! !$omp parallel default( shared ) &
+! !$omp private( ij, dummy,srt,end,siz )
             call TLab_OMP_PARTITION(field_sz, srt, end, siz)
 
             dummy = locProps%vector(2)
@@ -137,7 +137,7 @@ contains
                 r(ii, 1) = r(ii, 1) + dummy*(geo_w - u(ii, 3))
                 r(ii, 3) = r(ii, 3) + dummy*(u(ii, 1) - geo_u)
             end do
-!$omp end parallel
+! !$omp end parallel
         end select
 
     end subroutine Rotation_Coriolis
