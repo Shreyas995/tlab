d3544d2a4712028d7f1e5a27bf858d5160136d26
Shreyas <shreyasdeshpande16@gmail.com>
2025-08-27 12:54:45 +0200

    Pulled Tlab_Allocate_Real_LONG outside NO_ASSUMED_RANKS

--- src/base/tlab_memory.f90
+++ src/base/tlab_memory.f90
@@ -300,6 +300,21 @@ contains
 
     ! ######################################################################
     ! ######################################################################
+! ### DOUBLE ALLOCATION ROUTINES FOR LARGE 1D ARRAYS
+    subroutine Tlab_Allocate_Real_LONG(C_FILE_LOC, a, dims, s)
+        character(len=*), intent(in) :: C_FILE_LOC, s
+        real(8), allocatable, intent(inout) :: a(:)
+        integer(8), intent(in) :: dims(1)
+
+        !#####################################################################
+        call Tlab_Allocate_Log_LONG(lfile, dims, s)
+        allocate (a(dims(1)), stat=ierr)
+        call TLAB_ALLOCATE_ERR(C_FILE_LOC, efile, s)
+
+    end subroutine Tlab_Allocate_Real_LONG
+
+    ! ######################################################################
+    ! ######################################################################
 #ifndef NO_ASSUMED_RANKS
     subroutine TLab_Allocate_Real(C_FILE_LOC, a, dims, s)
 
@@ -327,19 +342,6 @@ contains
         call TLAB_ALLOCATE_ERR(C_FILE_LOC, efile, s)
 
     end subroutine TLab_Allocate_Real
-
-! ### DOUBLE ALLOCATION ROUTINES FOR LARGE 1D ARRAYS
-    subroutine Tlab_Allocate_Real_LONG(C_FILE_LOC, a, dims, s)
-        character(len=*), intent(in) :: C_FILE_LOC, s
-        real(8), allocatable, intent(inout) :: a(:)
-        integer(8), intent(in) :: dims(1)
-
-        !#####################################################################
-        call Tlab_Allocate_Log_LONG(lfile, dims, s)
-        allocate (a(dims(1)), stat=ierr)
-        call TLAB_ALLOCATE_ERR(C_FILE_LOC, efile, s)
-
-    end subroutine Tlab_Allocate_Real_LONG
 
     ! ######################################################################
     ! ######################################################################
