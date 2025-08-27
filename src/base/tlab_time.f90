dc3b3856b15b906009e9ea2e72af16294298404f
Shreyas <shreyasdeshpande16@gmail.com>
2025-08-27 10:28:15 +0200

    Added time varaibles to measure performance

--- src/base/tlab_time.f90
+++ src/base/tlab_time.f90
@@ -5,4 +5,10 @@ module TLab_Time
     integer(wi) :: itime                ! iteration number
     real(wp) :: rtime                   ! physical time
 
+! ###################################################################
+! APU profiling timings
+! ###################################################################    
+    real(wp) :: trans_time, tridss_time, tridpss_time
+    real(wp) :: mat5dantisym_time,mat5dsym_time,mat3dadd_time,mat3d_time, t_map_in, t_compute, t_map_out
+
 end module TLab_Time
