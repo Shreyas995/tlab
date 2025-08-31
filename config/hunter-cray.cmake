4c3c2d4af298bb30988e577d78720b5fc18a5761
Shreyas <shreyasdeshpande16@gmail.com>
2025-08-31 16:23:19 +0200

    bug fix

--- config/hunter-cray.cmake
+++ config/hunter-cray.cmake
@@ -16,21 +16,16 @@ if ( NOT PROFILE )
 endif()
 
 if (${PROFILE} STREQUAL "TRUE" )  
-   set(USER_profile_FLAGS "-g -h profile_generate")
+   set(USER_profile_FLAGS "-g ")#-h profile_generate")
 endif()
 
 if ( NOT ACCELERATE )
   set(ACCELERATE "FALSE")
 elseif( ${ACCELERATE} STREQUAL "TRUE" )
-  set(USER_APU_FLAGS "-fopenmp -L/opt/rh/gcc-toolset-12/root/usr/lib/gcc/x86_64-redhat-linux/12")
+  set(USER_APU_FLAGS "-fopenmp ")#-L/opt/rh/gcc-toolset-12/root/usr/lib/gcc/x86_64-redhat-linux/12")
   add_definitions(-DUSE_APU)
 endif() 
 
-if ( ${DEBUG} STREQUAL "TRUE" )
-   set(USER_DEBUG_FLAG   "-O0 -g -debug -ffpe-trap=all -Rb -Rchk -Rmem -h bounds")
-   message(WARNING "Compiling in DEBUG mode!")
-endif()
- 
 # compiler for parallel build	  
 if ( ${BUILD_TYPE} STREQUAL "PARALLEL" )
    set(ENV{FC} ftn) # instead of running "export FC=ftn" in the terminal
@@ -48,7 +43,7 @@ else( ${BUILD_TYPE} STREQUAL "SERIAL" )
 endif()     
 
 # set(DRAGONEGG_FLAGS "-finline-aggressive -fslp-vectorize  -fmerge-all-constants") #  -mmadd4 -mfp64 -enable-strided-vectorization")
-set(USER_Fortran_FLAGS         "-eZ ${USER_OMP_FLAGS} ${USER_APU_FLAGS} ${USER_profile_FLAGS} ${USER_DEBUG_FLAG}") #-fallow-argument-mismatch from gnu-version10
+set(USER_Fortran_FLAGS         "-eZ ${USER_OMP_FLAGS} ${USER_APU_FLAGS} ${USER_profile_FLAGS} ") #-fallow-argument-mismatch from gnu-version10
 set(USER_Fortran_FLAGS_RELEASE "-hipa2 -hfp2 -hunroll2 -hfusion2 -hscalar1 -m4" ) #these will be ignored:  -fprefetch-loop-arrays --param prefetch-latency=300") 
 # Flag -eo is disabled to reduce compiler output: 
 # manual -eo/-do Display to stderr the optimization options the compiler used for this compilation. This is the same as specifying -h display_opt. Default: disabled
