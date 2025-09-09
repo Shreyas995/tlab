dad4053e1878005fb49f9db64fdf408532381b05
Shreyas <shreyasdeshpande16@gmail.com>
2025-09-09 15:41:27 +0200

    Debugging on APU system

--- src/fdm/fdm_derivative.f90
+++ src/fdm/fdm_derivative.f90
@@ -375,7 +375,8 @@ contains
         
         if (allocated(g%rhs)) then 
             PRINT *, 'FDM_Der2_CreateSystem:','Deallocating g%rhs'
-            ! PRINT *, g%rhs
+            print *, shape(g%rhs)
+            print *, g%rhs
             deallocate (g%rhs) ! error here
             PRINT *, 'FDM_Der2_CreateSystem:','Deallocated g%rhs'
         end if
