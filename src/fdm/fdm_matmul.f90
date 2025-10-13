f7837532723a752e7df117a3661fb32ba2bce2e6
Shreyas <shreyasdeshpande16@gmail.com>
2025-10-14 01:30:41 +0200

    Bug fix in offloading

--- src/fdm/fdm_matmul.f90
+++ src/fdm/fdm_matmul.f90
@@ -221,7 +221,7 @@ contains
                         !$omp end target data
                     else if (present(bcs_t)) then
                         ! f(1) contains the boundary condition
-                        !$omp target data default shared use_device_ptr(bcs_t, f, u)
+                        !$omp target data use_device_ptr(bcs_t, f, u)
                         !$omp target teams distribute parallel do collapse(2) private(i,k,n,pa,pb,pc,pd,pe,pf)
                         do i = 1, ilines
                             do k = 1, klines
@@ -239,7 +239,7 @@ contains
                         !$omp end target teams distribute parallel do
                         !$omp end target data
                     else
-                        !$omp target data default shared use_device_ptr(f, u)
+                        !$omp target data use_device_ptr(f, u)
                         !$omp target teams distribute parallel do collapse(2) private(i,k,n,pa,pb,pc,pd,pe,pf)
                         do i = 1, ilines
                             do k = 1, klines
@@ -259,7 +259,7 @@ contains
                     end if
                 else
                     if (present(bcs_b)) then
-                        !$omp target data default shared use_device_ptr(bcs_b, f, u)
+                        !$omp target data use_device_ptr(bcs_b, f, u)
                         !$omp target teams distribute parallel do collapse(2) private(i,k,n,pa,pb,pc,pd,pe,pf)
                         do i = 1, ilines
                             do k = 1, klines
@@ -279,7 +279,7 @@ contains
                         !$omp end target teams distribute parallel do
                         !$omp end target data
                     else
-                        !$omp target data default shared use_device_ptr(f, u)
+                        !$omp target data  use_device_ptr(f, u)
                         !$omp target teams distribute parallel do collapse(2) private(i,k,n,pa,pb,pc,pd,pe,pf)
                         do i = 1, ilines
                             do k = 1, klines
@@ -302,7 +302,7 @@ contains
             else
                 if (any([BCS_MAX, BCS_BOTH] == ibc)) then
                     if (present(bcs_t)) then
-                        !$omp target data default shared use_device_ptr(bcs_t, f, u)
+                        !$omp target data use_device_ptr(bcs_t, f, u)
                         !$omp target teams distribute parallel do collapse(2) private(i,k,n,pa,pb,pc,pd,pe,pf)
                         do i = 1, ilines
                             do k = 1, klines
@@ -321,7 +321,7 @@ contains
                         !$omp end target teams distribute parallel do
                         !$omp end target data
                     else
-                        !$omp target data default shared use_device_ptr(f, u)
+                        !$omp target data use_device_ptr(f, u)
                         !$omp target teams distribute parallel do collapse(2) private(i,k,n,pa,pb,pc,pd,pe,pf)
                         do i = 1, ilines
                             do k = 1, klines
@@ -340,7 +340,7 @@ contains
                         !$omp end target data
                     end if
                 else
-                    !$omp target data default shared use_device_ptr(bcs_t, f, u)
+                    !$omp target data use_device_ptr(bcs_t, f, u)
                     !$omp target teams distribute parallel do collapse(2) private(i,k,n,pa,pb,pc,pd,pe,pf)
                     do i = 1, ilines
                         do k = 1, klines
