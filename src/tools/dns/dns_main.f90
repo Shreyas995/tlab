b40c191b582a19c415ac796d8ed2678df183708a
Shreyas <shreyasdeshpande16@gmail.com>
2025-10-31 14:14:42 +0100

    debugging parallel run

--- src/tools/dns/dns_main.f90
+++ src/tools/dns/dns_main.f90
@@ -50,10 +50,10 @@ program DNS
     use Avg_Spatial, only: IO_READ_AVG_SPATIAL, IO_WRITE_AVG_SPATIAL
     implicit none
     save
-    print *, ' loaded use statements'
 #ifdef USE_APU
     !$omp requires unified_shared_memory
 #endif
+    print *, ' loaded use statements'
 
     ! -------------------------------------------------------------------
     character(len=32) fname, str
