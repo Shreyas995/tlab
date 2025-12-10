23914ef7fed8a42f437153df54267843162d3463
Shreyas <shreyasdeshpande16@gmail.com>
2025-12-10 10:34:34 +0100

    debug for parallel and serial

--- src/base/tlab_debug.f90
+++ src/base/tlab_debug.f90
@@ -14,12 +14,18 @@
 module Tlab_Debug
     
     use  TLab_Constants, only : wp, wi
+#ifdef USE_MPI
     use  TLabMPI_VARS, only : ims_pro
+#endif
     implicit none
-
-    private
-    public :: TLab_Debug_Initialize, TLab_Debug_Print_1D, TLab_Debug_Print_2D, TLab_Debug_Print_3D
+    save
+    public :: TLab_Debug_Initialize, TLab_Debug_Print_1D, TLab_Debug_Print_2D, TLab_Debug_Print_3D, TLab_Debug_Print_4D
+    public :: TLab_Debug_Print_2D_c, TLab_Debug_Print_1D_c, TLab_Debug_Print_3D_c
+    public :: TLab_Debug_Print_2D_i
     integer(wi), parameter :: FILE_UNIT_BASE = 500
+#ifndef USE_MPI
+    integer(wi), parameter :: ims_pro = 0
+#endif
 contains 
     
     subroutine Tlab_Debug_Initialize()
@@ -31,7 +37,7 @@ contains
         ! Calculate thread-specific unit number
         unit_num = FILE_UNIT_BASE + ims_pro
 
-        write(filename, '(a, i0, a)') 'debug_thread_', ims_pro, '.log'
+        write(filename, '(a, i0, a)') 'debug_thread_n', ims_pro, '.log'
 
         inquire(unit=unit_num, opened=opened)
 
@@ -47,39 +53,178 @@ contains
 
     end subroutine Tlab_Debug_Initialize
 
-    subroutine TLab_Debug_Print_1D(msg, var)
+    subroutine TLab_Debug_Print_1D(msg, var, msg2)
         implicit none
         character(len=*), intent(in) :: msg
         real(wp), intent(in) :: var(:)
+        character(len=*), intent(in), optional :: msg2
         integer(wi) :: i
-        integer, parameter :: FILE_UNIT_BASE = 500
         integer :: unit_num
         unit_num = FILE_UNIT_BASE + ims_pro
-        write(unit_num, *) trim(msg), 'DEBUG (PE', ims_pro, '): ', sum(var)
+        if (present(msg2)) then
+            write(unit_num, *) trim(msg), 'DEBUG (PE', ims_pro, '): ', trim(msg2), ' ', sum(var)
+        else
+            write(unit_num, *) trim(msg), 'DEBUG (PE', ims_pro, '): ', sum(var)
+        end if
         flush(unit_num)
     end subroutine TLab_Debug_Print_1D
 
-    subroutine TLab_Debug_Print_2D(msg, var)
+    subroutine TLab_Debug_Print_2D(msg, var, msg2)
         character(len=*), intent(in) :: msg
         real(wp), intent(in) :: var(:,:)
+        character(len=*), intent(in), optional :: msg2
         integer(wp) :: i
-        do i = 0,3
-            if (i == ims_pro) then
-                write(*,*) trim(msg), 'DEBUG (PE', ims_pro, '): ', sum(var)
-            end if
-        end do
+        integer :: unit_num
+        unit_num = FILE_UNIT_BASE + ims_pro
+        if (present(msg2)) then
+            write(unit_num, *) trim(msg), 'DEBUG (PE', ims_pro, '): ', trim(msg2), ' ', sum(var)
+        else
+            write(unit_num, *) trim(msg), 'DEBUG (PE', ims_pro, '): ', sum(var)
+        end if
+        flush(unit_num)
     end subroutine TLab_Debug_Print_2D
 
-    subroutine TLab_Debug_Print_3D(msg, var)
+    subroutine TLab_Debug_Print_3D(msg, var, msg2)
         character(len=*), intent(in) :: msg
         real(wp), intent(in) :: var(:,:,:)
+        character(len=*), intent(in), optional :: msg2
         integer(wp) :: i
-        do i = 0,3
-            if (i == ims_pro) then
-                write(*,*) trim(msg), 'DEBUG (PE', ims_pro, '): ', sum(var)
-            end if
-        end do
+        integer :: unit_num
+        unit_num = FILE_UNIT_BASE + ims_pro
+        if (present(msg2)) then
+            write(unit_num, *) trim(msg), 'DEBUG (PE', ims_pro, '): ', trim(msg2), ' ', sum(var)
+        else
+            write(unit_num, *) trim(msg), 'DEBUG (PE', ims_pro, '): ', sum(var)
+        end if
+        flush(unit_num)
     end subroutine TLab_Debug_Print_3D
+
+    subroutine TLab_Debug_Print_2D_c(msg, var, msg2)
+        character(len=*), intent(in) :: msg
+        complex(wp), intent(in) :: var(:,:)
+        character(len=*), intent(in), optional :: msg2
+
+        integer(wp) :: i
+        integer :: unit_num
+
+        unit_num = FILE_UNIT_BASE + ims_pro
+        if (present(msg2)) then
+            write(unit_num, *) trim(msg), 'DEBUG (PE', ims_pro, '): ', trim(msg2), ' ', sum(var)
+        else
+            write(unit_num, *) trim(msg), 'DEBUG (PE', ims_pro, '): ', sum(var)
+        end if
+        flush(unit_num)
+    end subroutine TLab_Debug_Print_2D_c
+
+    subroutine TLab_Debug_Print_1D_c(msg, var, msg2)
+        implicit none
+        character(len=*), intent(in) :: msg
+        complex(wp), intent(in) :: var(:)
+        character(len=*), intent(in), optional :: msg2
+
+        integer(wi) :: i
+        integer :: unit_num
+
+        unit_num = FILE_UNIT_BASE + ims_pro
+        if (present(msg2)) then
+            write(unit_num, *) trim(msg), 'DEBUG (PE', ims_pro, '): ', trim(msg2), ' ', sum(var)
+        else
+            write(unit_num, *) trim(msg), 'DEBUG (PE', ims_pro, '): ', sum(var)
+        end if
+        flush(unit_num)
+    end subroutine TLab_Debug_Print_1D_c
+
+     subroutine TLab_Debug_Print_3D_c(msg, var, msg2)
+        character(len=*), intent(in) :: msg
+        complex(wp), intent(in) :: var(:,:,:)
+        character(len=*), intent(in), optional :: msg2
+
+        integer(wp) :: i
+        integer :: unit_num
+
+        unit_num = FILE_UNIT_BASE + ims_pro
+        if (present(msg2)) then
+            write(unit_num, *) trim(msg), 'DEBUG (PE', ims_pro, '): ', trim(msg2), ' ', sum(var)
+        else
+            write(unit_num, *) trim(msg), 'DEBUG (PE', ims_pro, '): ', sum(var)
+        end if
+        flush(unit_num)
+    end subroutine TLab_Debug_Print_3D_c
+
+    subroutine TLab_Debug_Print_4D(msg, var, msg2)
+        character(len=*), intent(in) :: msg
+        real(wp), intent(in) :: var(:,:,:,:)
+        character(len=*), intent(in), optional :: msg2
+        integer(wp) :: i
+        integer :: unit_num
+        unit_num = FILE_UNIT_BASE + ims_pro
+        if (present(msg2)) then
+            write(unit_num, *) trim(msg), 'DEBUG (PE', ims_pro, '): ', trim(msg2), ' ', sum(var)
+        else
+            write(unit_num, *) trim(msg), 'DEBUG (PE', ims_pro, '): ', sum(var)
+        end if
+        flush(unit_num)
+    end subroutine TLab_Debug_Print_4D
+
+    subroutine TLab_Debug_Print_1D_i(msg, var, msg2)
+        character(len=*), intent(in) :: msg
+        integer(wi), intent(in) :: var(:)
+        character(len=*), intent(in), optional :: msg2
+        integer(wp) :: i
+        integer :: unit_num
+        unit_num = FILE_UNIT_BASE + ims_pro
+        if (present(msg2)) then
+            write(unit_num, *) trim(msg), 'DEBUG (PE', ims_pro, '): ', trim(msg2), ' ', sum(var)
+        else
+            write(unit_num, *) trim(msg), 'DEBUG (PE', ims_pro, '): ', sum(var)
+        end if
+        flush(unit_num)
+    end subroutine TLab_Debug_Print_1D_i
+
+    subroutine TLab_Debug_Print_2D_i(msg, var, msg2)
+        character(len=*), intent(in) :: msg
+        integer(wi), intent(in) :: var(:,:)
+        character(len=*), intent(in), optional :: msg2
+        integer(wp) :: i
+        integer :: unit_num
+        unit_num = FILE_UNIT_BASE + ims_pro
+        if (present(msg2)) then
+            write(unit_num, *) trim(msg), 'DEBUG (PE', ims_pro, '): ', trim(msg2), ' ', sum(var)
+        else
+            write(unit_num, *) trim(msg), 'DEBUG (PE', ims_pro, '): ', sum(var)
+        end if
+        flush(unit_num)
+    end subroutine TLab_Debug_Print_2D_i
+
+    subroutine TLab_Debug_Print_int(msg, var, msg2)
+        character(len=*), intent(in) :: msg
+        integer(wi), intent(in) :: var
+        character(len=*), intent(in), optional :: msg2
+        integer(wp) :: i
+        integer :: unit_num
+        unit_num = FILE_UNIT_BASE + ims_pro
+        if (present(msg2)) then
+            write(unit_num, *) trim(msg), 'DEBUG (PE', ims_pro, '): ', trim(msg2), ' ', var
+        else
+            write(unit_num, *) trim(msg), 'DEBUG (PE', ims_pro, '): ', var
+        end if
+        flush(unit_num)
+    end subroutine TLab_Debug_Print_int
+
+    subroutine TLab_Debug_Print_real(msg, var, msg2)
+        character(len=*), intent(in) :: msg
+        real(wi), intent(in) :: var
+        character(len=*), intent(in), optional :: msg2
+        integer(wp) :: i
+        integer :: unit_num
+        unit_num = FILE_UNIT_BASE + ims_pro
+        if (present(msg2)) then
+            write(unit_num, *) trim(msg), 'DEBUG (PE', ims_pro, '): ', trim(msg2), ' ', var
+        else
+            write(unit_num, *) trim(msg), 'DEBUG (PE', ims_pro, '): ', var
+        end if
+        flush(unit_num)
+    end subroutine TLab_Debug_Print_real
 
 end module Tlab_Debug
 !########################################################################
